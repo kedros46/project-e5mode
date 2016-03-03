b39cb2b30247b5247975895bd2129b88302f2d4b
kedros46 <brecht46@gmail.com>
2016-03-03 12:33:19 +0100

    all pages added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,51 @@
 /**
  * Created by brecht on 25/02/2016.
  */
+
 $(function(){
    console.log("ready");
 
+    $("input").change(function(){
+        console.log($(this).val());
+    });
 
-    $("#usergebdate").submit(confirmuserinfo)
+    $(".userinfo").submit(confirmuserinfo);
+    $("#gegevens").bind("click", "h2, p", changeInput)
 });
 
 
 function confirmuserinfo() {
     var person = {
-        name: $("#naam").val();
-    }
+        voornaam: $("#naam").val(),
+        achternaam: $("#achternaam").val(),
+        adres: {
+            straat: $("#straat").val(),
+            huisnr: $("#huisnummer").val(),
+            postcode: $("#postcode").val(),
+            gemeente: $("#gemeente").val()
+        },
+        geslacht: $("input[type='radio']:checked").data("geslacht"),
+        geboortedatum: $("#geboortedatum").val()
+    };
+
+    console.log(person);
+
+    /*
+    $.ajax({
+        url: $(this).attr('action'),
+        type: "POST",
+        dataType: "JSON",
+        data: person
+    }).done(function(response){
+        console.log(response);
+    }).fail(function(xhr, message, error){
+        console.log(message, error);
+        //generate the fail screen
+    });
+    */
+}
+
+
+function changeInput() {
+
 }
